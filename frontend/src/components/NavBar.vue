4160fd2ba1e7511bc6ddf26cc51d4fef0da7ac2d
estern <stern.er@husky.neu.edu>
2020-02-08 13:25:55 -0500

    basic navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-brand href="#">Co-op Reviews</b-navbar-brand>
+      <b-navbar-nav>
+          <b-navbar-item href="#"> About </b-navbar-item>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto">
+          <b-navbar-item href="#"> Log In </b-navbar-item>
+      </b-navbar-nav>
     </b-navbar>
   </div>
 </template>
