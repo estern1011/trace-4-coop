4507f61098ed78c43fdf35652e6461151d596388
estern <stern.er@husky.neu.edu>
2020-02-08 15:26:54 -0500

    api hitting!

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,6 +11,7 @@ import NavBar from "./components/NavBar.vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+
 //import companies_page from "./constants.js"
 
 Vue.use(BootstrapVue);
@@ -20,7 +21,8 @@ export default {
   name: "App",
   components: {
     NavBar
-  }
+  },
+
 };
 </script>
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,8 +1,25 @@
 <template>
-  <p>homepage</p>
+  <div id="app">
+    <p>homepage</p>
+    {{ this.info }}
+  </div>
 </template>
 <script>
-export default { name: "Home" };
+import axios from "axios";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      info: {}
+    }
+  },
+  mounted() {
+    axios
+      .get("http://localhost:5000/companies")
+      .then(response => (this.info = response.data));
+  }
+};
 </script>
 <style>
 </style>
