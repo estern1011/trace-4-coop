4f890f65d3424c73e36907b3abea497e6b3d0612
estern <stern.er@husky.neu.edu>
2020-02-08 17:09:42 -0500

    added routing for positions

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,11 +1,14 @@
 import About from './views/About'
 import Home from './views/Home'
 import CompanyPage from './views/CompanyPage'
+import Position from './views/Position'
+
 
 const routes = [
   { path: "/", component: Home },
   { path: "/about", component: About },
-  { path: "/companies/:company_name", name: 'companies', component: CompanyPage}
+  { path: "/companies/:company_name", name: 'companies', component: CompanyPage},
+  { path: "/companies/:company_name/position/:position_name", name: 'position', component: Position}
 ];
 
 export default routes
---
+++ frontend/src/views/Position.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Hello world</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  }
+};
+</script>
+
+<style>
+</style>
