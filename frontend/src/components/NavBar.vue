9c11af9233f67d31cb955d722755d3f92f6b713c
estern <stern.er@husky.neu.edu>
2020-02-08 12:37:09 -0500

    broken navbar

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
