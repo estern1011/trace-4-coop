cd87c7a88a382ad9da06d8d41270be588e77f096
estern <stern.er@husky.neu.edu>
2020-02-09 00:56:06 -0500

    Update Position.vue

--- frontend/src/views/Position.vue
+++ frontend/src/views/Position.vue
@@ -1,7 +1,33 @@
 <template>
   <div>
-    <h2> Reviews for {{ this.company_name }}'s {{ this.position_name }} position </h2>
-    <pre><code>{{this.info}}</code></pre>
+    <h3>Reviews for {{ this.company_name }}'s {{ this.position_name }} position</h3>
+    <!-- <input type="checkbox" id="interview" v-model="interview">
+    <label for="interview">Interviews  </label>    
+    |
+    <input type="checkbox" id="coop" v-model="coop">
+    <label for="coop">Co-op experiences</label>-->
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col> <h5> Type of review </h5> </b-col>
+        <b-col> <h5> Offer? </h5> </b-col>
+        <b-col> <h5> Happy with offer? </h5> </b-col>
+        <b-col> <h5> Review message </h5> </b-col>
+      </b-row>
+      <b-row v-for="item in this.info" :key="item._id">
+        <b-col>{{ item.type }}</b-col>
+        <b-col>
+          <p>{{ item.offer }}</p>
+        </b-col>
+        <b-col>
+          <p>{{ item.happy }}</p>
+        </b-col>
+        <b-col>{{ item.message }}</b-col>
+      </b-row>
+    </b-container>
+
+    <!-- <pre><code>{{this.info}}</code></pre> -->
+    <!-- <h2>{{ this.interview }} </h2>
+    <h3>{{ this.coop }} </h3>-->
   </div>
 </template>
 
@@ -13,6 +39,8 @@ export default {
   name: "Position",
   data() {
     return {
+      interview: true,
+      coop: true,
       info: {}
     };
   },
@@ -23,7 +51,9 @@ export default {
     this.position_name = this.$route.params.position_name;
 
     axios
-      .get(`http://localhost:5000/companies/${this.company_id}/position/${this.position_id}`)
+      .get(
+        `http://localhost:5000/companies/${this.company_id}/position/${this.position_id}`
+      )
       .then(response => (this.info = response.data.data));
 
     console.log(this.company_id);
