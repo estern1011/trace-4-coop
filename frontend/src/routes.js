1c937931ea3dcfe482c11be906d04e9dc642aad9
estern <stern.er@husky.neu.edu>
2020-02-08 21:27:49 -0500

    Create routes.js

---
+++ frontend/src/routes.js
@@ -0,0 +1,17 @@
+import About from './views/About'
+import Home from './views/Home'
+import InterviewForm from './views/InterviewForm'
+import CoopForm from './views/CoopForm'
+import CompanyPage from './views/CompanyPage'
+import Thanks from './views/Thanks'
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/about", component: About },
+  { path: "/interview", component: InterviewForm },
+  { path: "/coop", component: CoopForm },
+  { path: "/companies/:company_name", name: 'companies', component: CompanyPage},
+  { path: "/thanks", component: Thanks}
+];
+
+export default routes
