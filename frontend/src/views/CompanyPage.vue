b0977435d00cb031b67c93bd4b173adc55260e94
estern <stern.er@husky.neu.edu>
2020-02-09 00:38:11 -0500

    full flow

--- frontend/src/views/CompanyPage.vue
+++ frontend/src/views/CompanyPage.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <h2>{{this.name}}</h2>
+    <h2>{{this.company_name}}</h2>
     <p>
       Don't see your position? Add your position:
       <router-link
-        :to="{ name: `positions`, params: {company_name: this.name, id: this.company_id}}"
+        :to="{ name: `positions`, params: {company_name: this.company_name, id: this.company_id}}"
       >here</router-link>.
     </p>
     <b-container class="bv-example-row">
@@ -15,16 +15,21 @@
         <b-col>
           <h5>Location</h5>
         </b-col>
+        <b-col></b-col>
       </b-row>
       <b-row v-for="item in this.info" :key="item._id">
         <b-col>
-          <p><router-link :to="{ name: `reviews`, params: {company_name: this.name, position_name: item.position_name, 
-          company_id: this.company_id, position_id: item._id}}">
+          <p>
+            <!-- <router-link :to="{ name: `reviews`, params: {company_name: this.company_name, position_name: item.position_name, 
+            company_id: this.company_id, position_id: item._id}}">-->
             {{item.position_name}}
-            </router-link>
-            </p>
+            <!-- </router-link> -->
+          </p>
         </b-col>
         <b-col>{{ item.location }}</b-col>
+        <b-col>
+            <button v-on:click="goToReview(item.position_name, item._id)">Go to reviews</button>
+        </b-col>
       </b-row>
     </b-container>
   </div>
@@ -34,27 +39,35 @@
 import axios from "axios";
 
 export default {
+  props: ["company_id", "company_name"],
   name: "Home",
   data() {
     return {
-      company_id: "",
-      info: {},
-      name: ""
+      info: {}
     };
+  },
+  methods: {
+    goToReview(pos_name, pos_id) {
+      this.$router.push({ name: 'reviews', params: {company_name: this.company_name, 
+            company_id: this.company_id, position_name: pos_name, position_id: pos_id} })
+    }
   },
   mounted() {
     this.company_id = this.$route.params.id;
+    this.company_name = this.$route.params.company_name;
     console.log(this.company_id);
 
     axios
       .get(`http://localhost:5000/companies/${this.company_id}`)
       .then(response => (this.info = response.data.data));
 
-    axios.get("http://localhost:5000/companies").then(response => {
-      this.name = response.data.data.filter(company => {
-        return company._id === this.company_id;
-      })[0].company_name;
-    });
+    // axios.get("http://localhost:5000/companies").then(response => {
+    //   this.name = response.data.data.filter(company => {
+    //     return company._id === this.company_id;
+    //   })[0].company_name;
+    // // });
+    // console.log(this.name)
+    // console.log(this.company_id)
   }
 };
 </script>
--- frontend/src/views/Position.vue
+++ frontend/src/views/Position.vue
@@ -1,14 +1,35 @@
 <template>
   <div>
-    <p>Hello world</p>
+    <h2> Reviews for {{ this.company_name }}'s {{ this.position_name }} position </h2>
+    <pre><code>{{this.info}}</code></pre>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
+  props: ["company_id", "company_name", "position_id", "position_name"],
+  name: "Position",
   data() {
     return {
+      info: {}
     };
+  },
+  mounted() {
+    this.company_id = this.$route.params.company_id;
+    this.company_name = this.$route.params.company_name;
+    this.position_id = this.$route.params.position_id;
+    this.position_name = this.$route.params.position_name;
+
+    axios
+      .get(`http://localhost:5000/companies/${this.company_id}/position/${this.position_id}`)
+      .then(response => (this.info = response.data.data));
+
+    console.log(this.company_id);
+    console.log(this.company_name);
+    console.log(this.position_id);
+    console.log(this.position_name);
   }
 };
 </script>
