96d0a550e8c7b2dbf8738ab927465f5b55f88790
jzhang1223 <zhang.just@husky.neu.edu>
2020-02-08 22:22:01 -0500

    Remove card and add link to form posting

--- frontend/src/views/CompanyPage.vue
+++ frontend/src/views/CompanyPage.vue
@@ -1,31 +1,29 @@
 <template>
   <div>
     <p>{{ $route.params.id }}</p>
-    <b-card
-      :title= "this.name" 
-      img-src="https://picsum.photos/600/300/?image=25"
-      img-alt="Image"
-      img-top
-      tag="article"
-      class="mb-2"
-    >
-      <b-container class="bv-example-row">
-        <b-row>
-          <b-col>
-            <h5>Position</h5>
-          </b-col>
-          <b-col>
-            <h5>id</h5>
-          </b-col>
-        </b-row>
-        <b-row v-for="item in this.info" :key="item._id">
-          <b-col>
-            <p>{{item.position}}</p>
-          </b-col>
-          <b-col>{{ item._id }}</b-col>
-        </b-row>
-      </b-container>
-    </b-card>
+    <p>{{this.name}}</p>
+    <p>
+      Don't see your position? Add your position
+      <router-link
+        :to="{ name: `companies/${$route.params.id}`, params: {company_name: this.name, id: $route.params.id}}"
+      >here</router-link>.
+    </p>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>
+          <h5>Position</h5>
+        </b-col>
+        <b-col>
+          <h5>id</h5>
+        </b-col>
+      </b-row>
+      <b-row v-for="item in this.info" :key="item._id">
+        <b-col>
+          <p>{{item.position}}</p>
+        </b-col>
+        <b-col>{{ item._id }}</b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -41,7 +39,6 @@ export default {
     };
   },
   mounted() {
-
     const company_id = this.$route.params.id;
     console.log(company_id);
 
@@ -49,14 +46,13 @@ export default {
       .get(`http://localhost:5000/companies/${company_id}`)
       .then(response => (this.info = response.data.data));
 
-    axios
-      .get("http://localhost:5000/companies")
-      .then(response => {
-        this.name = response.data.data.filter((company) => {return company._id === company_id})[0].company_name;
-      })
+    axios.get("http://localhost:5000/companies").then(response => {
+      this.name = response.data.data.filter(company => {
+        return company._id === company_id;
+      })[0].company_name;
+    });
   }
 };
-
 </script>
 
 <style>
